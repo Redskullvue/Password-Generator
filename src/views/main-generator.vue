edd79366b4bcf463fe0f4e5945995e61e338b114
redskull422 <kofifa.pg@gmail.com>
2022-12-27 14:01:41 +0330

    Feature : Password Generator container added

---
+++ src/views/main-generator.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-col spacearound>
+    <password-input class="password-input" />
+  </v-col>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+// Components
+import passwordInput from "../components/password-input.vue";
+
+export default defineComponent({
+  name: "mainGenerator",
+
+  components: {
+    passwordInput,
+  },
+});
+</script>
+<style scoped>
+.password-input {
+  margin: 20px 40px;
+  box-shadow: 1px 1px 10px 1px gray;
+}
+</style>
